68e9449041da60b00456ae7878e24867b8991520
Fer457 <fernandonevothernandez@gmail.com>
2022-11-17 16:55:28 +0100

    if statements replaced with when statements

--- app/src/main/java/com/example/practica2fer/MainActivity.kt
+++ app/src/main/java/com/example/practica2fer/MainActivity.kt
@@ -17,34 +17,40 @@ class MainActivity : AppCompatActivity() {
 
         // funcion del boton, si algun campo esta vacio saca el toast y hace return
         binding.btnCalc.setOnClickListener {
-            if (binding.edPeso.text.isNullOrEmpty() || binding.edAltura.text.isNullOrEmpty()){
-                Toast.makeText(this, "Los campos Peso y Altura no deben estar vacíos para realizar el cálculo.", Toast.LENGTH_SHORT).show()
+            if (binding.edPeso.text.isNullOrEmpty() || binding.edAltura.text.isNullOrEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Los campos Peso y Altura no deben estar vacíos para realizar el calculo.",
+                    Toast.LENGTH_SHORT
+                ).show()
                 return@setOnClickListener
             }
 
             // si no recoge las variables de los ed y hace el calculo
             val peso = binding.edPeso.text.toString().toDouble()
-            val altura = binding.edAltura.text.toString().toDouble()/100
-            val result = peso/(altura * altura)
+            val altura = binding.edAltura.text.toString().toDouble() / 100
+            val result = peso / (altura * altura)
 
             // si el cb hombre esta checkeado...
-            val resultText = if (binding.rb1.isChecked){
-                if (result < 18.5) "Peso inferior al normal"
-                else if (result in 18.5..24.9) "Normal"
-                else if (result in 24.9..29.9) "Sobrepeso"
-                else "Obesidad"
-            // si es el cb de mujer...
+            val resultText = if (binding.rb1.isChecked) {
+                when (result) {
+                    in 0.0..18.5 -> "Peso inferior al normal"
+                    in 18.5..24.9 -> "Normal"
+                    in 24.9..29.9 -> "Sobrepeso"
+                    else -> "Obesidad"
+                }
+                // si es el cb de mujer...
             } else {
-                if (result < 18.5) "Peso inferior al normal"
-                else if (result in 18.5..23.9) "Normal"
-                else if (result in 24.0..28.9) "Sobrepeso"
-                else "Obesidad"
+                when (result) {
+                    in 0.0..18.5 -> "Peso inferior al normal"
+                    in 18.5..23.9 -> "Normal"
+                    in 24.0..28.9 -> "Sobrepeso"
+                    else -> "Obesidad"
+                }
             }
-
             // pone los resultados en los tv
             binding.tvResultado.text = String.format("%.2f", result)
             binding.tvResText.text = resultText
-
         }
     }
 }
